9d41c6b1f6740638816902b3b430ce0c0a710522
chuthuong2004 <19h1120035@sv.ut.edu.vn>
2023-04-23 00:20:14 +0700

    update block title component

--- src/components/BlockTitle/BlockTitle.tsx
+++ src/components/BlockTitle/BlockTitle.tsx
@@ -1,21 +1,18 @@
 import classNames from 'classnames/bind';
-import { Link } from 'react-router-dom';
 import styles from './BlockTitle.module.scss';
-import PropTypes from 'prop-types';
-import React from 'react';
+import React, { memo } from 'react';
 const cx = classNames.bind(styles);
 
-
 export type Props = {
-    title: string;
-    strong: string;
-}
-const BlockTitle: React.FC<Props> = ({ title, strong }) => {
-    return (
-        <div className={cx('container')}>
-            <div className={cx('block-title')}>{title}</div>
-            <strong>{strong}</strong>
-        </div>
-    );
+  title: string;
+  strong: string;
 };
-export default BlockTitle;
+const BlockTitle: React.FC<Props> = ({ title, strong }) => {
+  return (
+    <div className={cx('container')}>
+      <div className={cx('block-title')}>{title}</div>
+      <strong>{strong}</strong>
+    </div>
+  );
+};
+export default memo(BlockTitle);
