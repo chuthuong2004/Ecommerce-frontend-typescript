4e3831d0fd67ee04d7e409dfbab18656e063ac3e
chuthuong2004 <19h1120035@sv.ut.edu.vn>
2023-04-23 00:22:00 +0700

    clean code in address page

--- src/pages/Account/components/Address/Address.tsx
+++ src/pages/Account/components/Address/Address.tsx
@@ -11,103 +11,98 @@ import { useAppSelector } from '../../../../app/hooks';
 import { selectAuth } from '../../../../features/authSlice';
 import { useAddAddressMutation } from '../../../../services/authApi';
 import FormEdit from '../../../../components/FormEdit';
+import { Helmet } from '../../../../components';
 const cx = classNames.bind(styles);
 type Props = {};
 const Address: React.FC<Props> = ({}) => {
   const { user } = useAppSelector(selectAuth);
-  const [isOpenAddAddress, setIsOpenAddAddress] = useState(false);
-  const [isOpenEditAddress, setIsOpenEditAddress] = useState(false);
+  const [openPopup, setOpenPopup] = useState<'edit' | 'address' | ''>('');
   const [addressEdit, setAddressEdit] = useState<IAddressUser | undefined>(undefined);
 
   useEffect(() => {
-    !isOpenEditAddress && setAddressEdit(undefined);
-  }, [isOpenEditAddress]);
-  useEffect(() => {
-    document.title = 'Địa chỉ - Koga-clothes.shop';
-  }, []);
+    openPopup !== 'edit' && setAddressEdit(undefined);
+  }, [openPopup]);
   const handleClickEdit = (address: IAddressUser) => {
     setAddressEdit(address);
-    setIsOpenEditAddress(true);
+    setOpenPopup('edit');
   };
-
-  const handleClosePopupEdit = useCallback(() => setIsOpenEditAddress(false), []);
-  const handleClosePopupAdd = useCallback(() => setIsOpenAddAddress(false), []);
+  const handleOpenPopupAddress = useCallback(() => setOpenPopup('address'), []);
+  const handleClosePopup = useCallback(() => setOpenPopup(''), []);
   return (
-    <div className={cx('account-address')}>
-      <PopUp
-        trigger={<></>}
-        isOpen={isOpenAddAddress}
-        handleClose={() => setIsOpenAddAddress(false)}
-        position="center"
-      >
-        <FormEdit
-          title="Thêm địa chỉ"
-          isOpen={isOpenAddAddress}
-          handleClosePopUp={handleClosePopupAdd}
-          action="address"
-        />
-      </PopUp>
-      {user?.addresses && user.addresses.length > 0 ? (
-        <>
-          <div className={cx('add-address')}>
-            <Button
-              className={cx('btn-add-address')}
-              leftIcon={<PlusStrongIcon color="#ffffff" />}
-              primary
-              onClick={() => setIsOpenAddAddress(true)}
-            >
-              Thêm Địa chỉ
-            </Button>
-          </div>
-          <div className={cx('addresses')}>
-            {user.addresses.map((item: IAddressUser) => (
-              <div key={item._id} className={cx('address')}>
-                <div className={cx('address-name', item.isDefault && 'default-address')}>
-                  <div className={cx('name')}>
-                    <span>{`${item.firstName} ${item.lastName}`}</span>
-                    <div onClick={() => handleClickEdit(item)} className={cx('edit-icon')}>
-                      <EditIcon />
-                    </div>
-                  </div>
-                  <div className={cx('btn-default')}>
-                    <Button primary className={cx('btn')}>
-                      Địa chỉ mặc định
-                    </Button>
-                  </div>
-                </div>
-                <p className={cx('phone')}>{item.phone}</p>
-                <p className={cx('delivery-address')}>
-                  {`${item.address}, ${item.ward}, ${item.district}, ${item.province}`}
-                </p>
-              </div>
-            ))}
-          </div>
-          <PopUp
-            trigger={<></>}
-            isOpen={isOpenEditAddress}
-            handleClose={() => setIsOpenEditAddress(false)}
-            position="center"
-          >
+    <Helmet title="Địa chỉ www.kogaclothes.shop">
+      <div className={cx('account-address')}>
+        <PopUp
+          trigger={<></>}
+          isOpen={openPopup ? true : false}
+          handleClose={handleClosePopup}
+          position="center"
+        >
+          {openPopup === 'address' && (
             <FormEdit
-              address={addressEdit}
-              isOpen={isOpenEditAddress}
-              title="Cập nhật địa chỉ"
-              handleClosePopUp={handleClosePopupEdit}
+              title="Thêm địa chỉ"
+              isOpen={openPopup === 'address'}
+              handleClosePopUp={handleClosePopup}
               action="address"
             />
-          </PopUp>
-        </>
-      ) : (
-        <EmptyContent
-          icon={<LocationEmptyIcon />}
-          titleBtn="Thêm địa chỉ"
-          content1="Không có địa chỉ lưu sẵn"
-          content2="Lưu sẵn địa chỉ ở đây để rút ngắn thời gian đặt hàng."
-          iconBtn={<PlusStrongIcon color="#ffffff" />}
-          onClickButton={() => setIsOpenAddAddress(true)}
-        />
-      )}
-    </div>
+          )}
+          {openPopup === 'edit' && (
+            <FormEdit
+              address={addressEdit}
+              isOpen={openPopup === 'edit'}
+              title="Cập nhật địa chỉ"
+              handleClosePopUp={handleClosePopup}
+              action="address"
+            />
+          )}
+        </PopUp>
+        {user?.addresses && user.addresses.length > 0 ? (
+          <>
+            <div className={cx('add-address')}>
+              <Button
+                className={cx('btn-add-address')}
+                leftIcon={<PlusStrongIcon color="#ffffff" />}
+                primary
+                onClick={handleOpenPopupAddress}
+              >
+                Thêm Địa chỉ
+              </Button>
+            </div>
+            <div className={cx('addresses')}>
+              {user.addresses.map((item: IAddressUser) => (
+                <div key={item._id} className={cx('address')}>
+                  <div className={cx('address-name', item.isDefault && 'default-address')}>
+                    <div className={cx('name')}>
+                      <span>{`${item.firstName} ${item.lastName}`}</span>
+                      <div onClick={() => handleClickEdit(item)} className={cx('edit-icon')}>
+                        <EditIcon />
+                      </div>
+                    </div>
+                    <div className={cx('btn-default')}>
+                      <Button primary className={cx('btn')}>
+                        Địa chỉ mặc định
+                      </Button>
+                    </div>
+                  </div>
+                  <p className={cx('phone')}>{item.phone}</p>
+                  <p className={cx('delivery-address')}>
+                    {`${item.address}, ${item.ward}, ${item.district}, ${item.province}`}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </>
+        ) : (
+          <EmptyContent
+            icon={<LocationEmptyIcon />}
+            titleBtn="Thêm địa chỉ"
+            content1="Không có địa chỉ lưu sẵn"
+            content2="Lưu sẵn địa chỉ ở đây để rút ngắn thời gian đặt hàng."
+            iconBtn={<PlusStrongIcon color="#ffffff" />}
+            onClickButton={handleOpenPopupAddress}
+          />
+        )}
+      </div>
+    </Helmet>
   );
 };
 
