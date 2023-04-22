e880eb6dd1b0bfdece7c36ee45f5e2e1d730b282
chuthuong2004 <19h1120035@sv.ut.edu.vn>
2023-04-23 00:20:51 +0700

    add helmet component

---
+++ src/components/Helmet/Helmet.tsx
@@ -0,0 +1,15 @@
+import React, { memo, useEffect } from 'react';
+
+interface HelmetProps {
+  title?: string;
+  children: JSX.Element;
+}
+const Helmet: React.FC<HelmetProps> = ({ title, children }) => {
+  useEffect(() => {
+    document.title = title ? title : 'www.kogaclothes.shop';
+  }, [title]);
+  console.log(title);
+  return children;
+};
+
+export default memo(Helmet);
